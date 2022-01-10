52acf703be1be4e8aeb1cb6de39b7614a75567bb
Oge Obubu <ogeobubu@gmail.com>
2022-01-10 10:56:11 +0100

    fix deploy issue

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.use(express.json());
 app.use("/api/images", imageRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("client/dist"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "./dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
   });
 }
 
